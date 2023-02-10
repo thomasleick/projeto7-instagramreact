7a42055fc136bafaee34caee8abd038a8d36894b
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-10 09:48:36 -0300

    User component 100% and working on Suggestions

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -2,9 +2,72 @@ import React from 'react';
 
 const Suggestions = () => {
     return (
-        <div>
-            
-        </div>
+<div className="sugestoes">
+                    <div className="titulo">
+                        Sugestões para você
+                        <div>Ver tudo</div>
+                    </div>
+
+                    <div className="sugestao">
+                        <div className="usuario">
+                        <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
+                        <div className="texto">
+                            <div className="nome">bad.vibes.memes</div>
+                            <div className="razao">Segue você</div>
+                        </div>
+                        </div>
+
+                        <div className="seguir">Seguir</div>
+                    </div>
+
+                    <div className="sugestao">
+                        <div className="usuario">
+                        <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
+                        <div className="texto">
+                            <div className="nome">chibirdart</div>
+                            <div className="razao">Segue você</div>
+                        </div>
+                        </div>
+
+                        <div className="seguir">Seguir</div>
+                    </div>
+
+                    <div className="sugestao">
+                        <div className="usuario">
+                        <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
+                        <div className="texto">
+                            <div className="nome">razoesparaacreditar</div>
+                            <div className="razao">Novo no Instagram</div>
+                        </div>
+                        </div>
+
+                        <div className="seguir">Seguir</div>
+                    </div>
+
+                    <div className="sugestao">
+                        <div className="usuario">
+                        <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
+                        <div className="texto">
+                            <div className="nome">adorable_animals</div>
+                            <div className="razao">Segue você</div>
+                        </div>
+                        </div>
+
+                        <div className="seguir">Seguir</div>
+                    </div>
+
+                    <div className="sugestao">
+                        <div className="usuario">
+                        <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
+                        <div className="texto">
+                            <div className="nome">smallcutecats</div>
+                            <div className="razao">Segue você</div>
+                        </div>
+                        </div>
+
+                        <div className="seguir">Seguir</div>
+                    </div>
+                    </div>
     );
 };
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 const User = () => {
+
+    const [user, setUser] = useState("catanacomics")
+    const [img, setImg] = useState("assets/img/catanacomics.svg")
+
+    const changeUser = () => {
+        const oldUser = user;
+        const newUser = prompt("Qual o seu nome?")
+            newUser.length > 0 ? setUser(newUser) : setUser(oldUser)
+    }
+
+    const changeImg = () => {
+        const oldImg = img;
+        const newImg = prompt("Qual o link da nova foto do perfil?")
+            newImg.length > 0 ? setImg(newImg) : setImg(oldImg)
+    }
+
     return (
-        <div>
-            
+        <div className="usuario">
+            <img src={img} alt="imagem de perfil" onClick={changeImg}/>
+            <div className="texto">
+                <span>
+                    <strong>{user}</strong>
+                    <ion-icon name="pencil" onClick={changeUser} />
+                </span>
+            </div>
         </div>
     );
 };
