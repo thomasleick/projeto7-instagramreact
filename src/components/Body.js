38244965a0ffdfcca067d2e423576a984c1e700f
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-10 09:17:48 -0300

    Body component 100%

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Stories from './Stories'
 import Posts from './Posts'
+import SideBar from './SideBar';
 
 const Body = () => {
     return (
@@ -9,94 +10,7 @@ const Body = () => {
                 <Stories />
                 <Posts />
             </div>
-
-                <div className="sidebar">
-                    <div className="usuario">
-                    <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-                    <div className="texto">
-                        <span>
-                        <strong>catanacomics</strong>
-                        <ion-icon name="pencil"></ion-icon>
-                        </span>
-                    </div>
-                    </div>
-
-                    <div className="sugestoes">
-                    <div className="titulo">
-                        Sugestões para você
-                        <div>Ver tudo</div>
-                    </div>
-
-                    <div className="sugestao">
-                        <div className="usuario">
-                        <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
-                        <div className="texto">
-                            <div className="nome">bad.vibes.memes</div>
-                            <div className="razao">Segue você</div>
-                        </div>
-                        </div>
-
-                        <div className="seguir">Seguir</div>
-                    </div>
-
-                    <div className="sugestao">
-                        <div className="usuario">
-                        <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
-                        <div className="texto">
-                            <div className="nome">chibirdart</div>
-                            <div className="razao">Segue você</div>
-                        </div>
-                        </div>
-
-                        <div className="seguir">Seguir</div>
-                    </div>
-
-                    <div className="sugestao">
-                        <div className="usuario">
-                        <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
-                        <div className="texto">
-                            <div className="nome">razoesparaacreditar</div>
-                            <div className="razao">Novo no Instagram</div>
-                        </div>
-                        </div>
-
-                        <div className="seguir">Seguir</div>
-                    </div>
-
-                    <div className="sugestao">
-                        <div className="usuario">
-                        <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
-                        <div className="texto">
-                            <div className="nome">adorable_animals</div>
-                            <div className="razao">Segue você</div>
-                        </div>
-                        </div>
-
-                        <div className="seguir">Seguir</div>
-                    </div>
-
-                    <div className="sugestao">
-                        <div className="usuario">
-                        <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
-                        <div className="texto">
-                            <div className="nome">smallcutecats</div>
-                            <div className="razao">Segue você</div>
-                        </div>
-                        </div>
-
-                        <div className="seguir">Seguir</div>
-                    </div>
-                    </div>
-
-                    <div className="links">
-                    Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
-                    Hashtags • Idioma
-                    </div>
-
-                    <div className="copyright">
-                    © 2021 INSTAGRAM DO FACEBOOK
-                    </div>
-                </div>
+            <SideBar />
         </main>
     );
 };
