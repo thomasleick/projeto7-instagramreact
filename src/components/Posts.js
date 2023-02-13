05f2d8e73184f25d4dcba0b1c85e0aef0cd532f4
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-02-13 08:50:01 -0300

    testing avaliador hub

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,8 +5,8 @@ import Post from './Post';
 const Posts = () => {
 
     const posts = [
-        { from: "adorable_animals", content:"bear", likedBy:"barked", likes:"85123", isVideo: true, isSaved: true, isLiked: true },
-        { from:"meowed", content:"gato-telefone", likedBy:"respondeai", likes:"101523", isVideo: false, isSaved: false, isLiked: false },
+        { from: "adorable_animals", content:"bear", likedBy:"barked", likes:"85123", isVideo: true, isSaved: false, isLiked: false },
+        { from:"meowed", content:"gato-telefone", likedBy:"respondeai", likes:"101523", isVideo: false, isSaved: true, isLiked: true },
         { from:"barked", content:"dog", likedBy:"adorable_animals", likes:"99159", isVideo: false},
     ];
 
